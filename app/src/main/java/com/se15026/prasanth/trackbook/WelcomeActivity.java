ef842b6432179082d08c80ad147a6f171899ff49
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-12 16:09:30 +0530

    Android project created

---
+++ app/src/main/java/com/se15026/prasanth/trackbook/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.se15026.prasanth.trackbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
