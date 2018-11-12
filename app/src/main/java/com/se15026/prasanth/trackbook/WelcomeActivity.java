aefe7feb7773938ea60c51fb50395e76c54b0a30
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-12 16:58:18 +0530

    Auto connect to HomeActivity is finished

--- app/src/main/java/com/se15026/prasanth/trackbook/WelcomeActivity.java
+++ app/src/main/java/com/se15026/prasanth/trackbook/WelcomeActivity.java
@@ -1,13 +1,27 @@
 package com.se15026.prasanth.trackbook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private static int time = 5000; //Assign 5 seconds to time variable
+    private Handler myHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        myHandler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, time);
+
     }
 }
