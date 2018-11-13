32f2b3ef29f2656f3ce219f351144cdc5e00369e
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-13 11:46:03 +0530

    Signup link added

--- app/src/main/java/com/se15026/prasanth/trackbook/HomeActivity.java
+++ app/src/main/java/com/se15026/prasanth/trackbook/HomeActivity.java
@@ -1,7 +1,7 @@
 package com.se15026.prasanth.trackbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +9,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
+
 }
--- app/src/main/java/com/se15026/prasanth/trackbook/LoginActivity.java
+++ app/src/main/java/com/se15026/prasanth/trackbook/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.se15026.prasanth.trackbook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        changeActivity();
+    }
+
+    public void changeActivity(){
+        Button signUp = (Button)findViewById(R.id.signUpBtn);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
