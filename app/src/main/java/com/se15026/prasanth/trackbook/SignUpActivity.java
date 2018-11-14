5924bb17651d02889f1885653916845faeabf5c4
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-14 16:29:52 +0530

    Signup cancel link added

--- app/src/main/java/com/se15026/prasanth/trackbook/SignUpActivity.java
+++ app/src/main/java/com/se15026/prasanth/trackbook/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.se15026.prasanth.trackbook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        changeActivity();
+    }
+
+    public void changeActivity(){
+        Button signUp = (Button)findViewById(R.id.cancel);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
