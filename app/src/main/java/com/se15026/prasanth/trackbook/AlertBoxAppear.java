bdfc715c528d2ac248d051103cc4c52157668734
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-21 18:42:22 +0530

    Alert Box Class created

---
+++ app/src/main/java/com/se15026/prasanth/trackbook/AlertBoxAppear.java
@@ -0,0 +1,29 @@
+package com.se15026.prasanth.trackbook;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AlertBoxAppear extends AppCompatActivity {
+
+    public void createAlertBox(){
+        /*AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+        alert.setTitle("Sample");
+        alert.setMessage("Change?");
+
+        alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Nothing Happened", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.create().show();*/
+    }
+}
