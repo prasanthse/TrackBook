e40d756899285fef0622309d2ab347ba2316b03a
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-11-29 19:38:36 +0530

    create account linked with firebase

--- app/src/main/java/com/se15026/prasanth/trackbook/SignUpActivity.java
+++ app/src/main/java/com/se15026/prasanth/trackbook/SignUpActivity.java
@@ -5,34 +5,51 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private DatabaseReference database;
+
+    private Button signUpCancel = (Button)findViewById(R.id.cancel);
+    private Button createAccount = (Button) findViewById(R.id.createAccount);
+    private EditText nameTxt = (EditText) findViewById(R.id.name);
+    private EditText phoneTxt = (EditText) findViewById(R.id.phone);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        changeActivity();
-    }
-
-    public void changeActivity(){
-        Button signUpCancel = (Button)findViewById(R.id.cancel);
-        Button createAccount = (Button) findViewById(R.id.createAccount);
+        database = FirebaseDatabase.getInstance().getReference();
 
         signUpCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(intent);
+                //alertDialog
+                backToHome();
             }
         });
 
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ToastMessage objToastAccountCreate = new ToastMessage("Hi Prasanth, your account is created",getApplicationContext());
+                String userName = nameTxt.getText().toString().trim();
+                String userPhoneNumber = phoneTxt.getText().toString().trim();
+
+                database.child("Login").child(userPhoneNumber).setValue(userName);
+
+                ToastMessage objToastAccountCreate = new ToastMessage("Hi "+userName+", your account is created",getApplicationContext());
+                backToHome();
             }
         });
+    }
+
+    private void backToHome(){
+        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
